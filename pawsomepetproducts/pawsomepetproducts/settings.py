c872fd593206d6e66d034807e1efb6a695f0a1fc
bithulmb <bithulmb@gmail.com>
2025-05-08 10:31:13 +0530

    requirements updated

--- pawsomepetproducts/pawsomepetproducts/settings.py
+++ pawsomepetproducts/pawsomepetproducts/settings.py
@@ -177,7 +177,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     ]
-STATIC_ROOT = "/var/www/pawsomepetproducts/static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 #Media files configuration
