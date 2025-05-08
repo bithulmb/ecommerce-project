9a6ffc317a3027e2407ed1afb93990efc19311dc
bithulmb <bithulmb@gmail.com>
2025-05-08 11:02:01 +0530

    user model manager error rectified

--- pawsomepetproducts/accounts/models.py
+++ pawsomepetproducts/accounts/models.py
@@ -1,33 +1,98 @@
 
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 # Create your models here.
 
 
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, first_name, last_name, phone_number=None, email, password=None):
+#         if not email:
+#             raise ValueError("User must have an email address")
+#         # if not username:
+#         #     raise ValueError('User must have an username')
+
+#         user = self.model(
+#             email=self.normalize_email(email),
+#             # username    = username,
+#             first_name=first_name,
+#             last_name=last_name,
+#             phone_number=phone_number,
+#         )
+
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+#     def create_superuser(self, first_name, last_name, email, password):
+#         user = self.create_user(
+#             email=self.normalize_email(email),
+#             # username    = username,
+#             password=password,
+#             first_name=first_name,
+#             last_name=last_name,
+#         )
+#         user.is_admin = True
+#         user.is_active = True
+#         user.is_staff = True
+#         user.is_superadmin = True
+#         user.save(using=self._db)
+#         return user
+
+
+# # custom user model for creating users
+# class CustomUser(AbstractBaseUser):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     # username        =models.CharField(max_length=50, unique=True)
+#     email = models.CharField(max_length=100, unique=True)
+#     phone_number = models.CharField(max_length=50, null=True)
+
+#     # required
+#     date_joined = models.DateTimeField(auto_now_add=True)
+#     last_login = models.DateTimeField(auto_now_add=True)
+#     is_admin = models.BooleanField(default=False)
+#     is_staff = models.BooleanField(default=False)
+#     is_superadmin = models.BooleanField(default=False)
+#     is_active = models.BooleanField(default=True)
+#     is_blocked = models.BooleanField(default=False)
+
+#     USERNAME_FIELD = "email"
+#     REQUIRED_FIELDS = ["first_name", "last_name"]
+
+#     objects = CustomUserManager()
+
+#     def __str__(self):
+#         return self.email
+
+#     def has_perm(self, perm, obj=None):
+#         return self.is_admin
+
+#     def has_module_perms(self, add_label):
+#         return True
+
+#     def full_name(self):
+#         return f"{self.first_name} {self.last_name}"
+
 class CustomUserManager(BaseUserManager):
-    def create_user(self, first_name, last_name, phone_number, email, password=None):
+    def create_user(self, first_name, last_name, email, phone_number=None, password=None):
         if not email:
-            raise ValueError("User must have an email address")
-        # if not username:
-        #     raise ValueError('User must have an username')
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
 
         user = self.model(
-            email=self.normalize_email(email),
-            # username    = username,
+            email=email,
             first_name=first_name,
             last_name=last_name,
             phone_number=phone_number,
         )
-
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, first_name, last_name, email, password):
         user = self.create_user(
-            email=self.normalize_email(email),
-            # username    = username,
+            email=email,
             password=password,
             first_name=first_name,
             last_name=last_name,
@@ -40,17 +105,15 @@ class CustomUserManager(BaseUserManager):
         return user
 
 
-# custom user model for creating users
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    # username        =models.CharField(max_length=50, unique=True)
-    email = models.CharField(max_length=100, unique=True)
-    phone_number = models.CharField(max_length=50, null=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=50, null=True, blank=True)
 
     # required
     date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superadmin = models.BooleanField(default=False)
@@ -66,15 +129,14 @@ class CustomUser(AbstractBaseUser):
         return self.email
 
     def has_perm(self, perm, obj=None):
-        return self.is_admin
+        return self.is_admin or self.is_superuser
 
-    def has_module_perms(self, add_label):
-        return True
+    def has_module_perms(self, app_label):
+        return self.is_admin or self.is_superuser
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
-
-
+    
 class Address(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=30, null=True)
